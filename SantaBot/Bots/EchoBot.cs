d1bd375e63c5e48c83369bf2eb268850719c05ae
Naama <naama.nalawala@gmail.com>
2023-12-28 16:26:21 +0400

    Santa bot code initialized

---
+++ SantaBot/Bots/EchoBot.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.18.1
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using SantaBot.Responses;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SantaBot.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var userMessage = turnContext.Activity.Text;
+            string replyText;
+
+            if (userMessage.Contains("weather"))
+            {
+                replyText = "I'm a bot, not a weather station 🌦️";
+            }
+            else
+            {
+                replyText = SarcasticResponse.GetRandomResponse();
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hey there, you unsuspecting victim of sibling shenanigans! As the new year unfolds, I've been bestowed with the honor of entertaining you with wit, sarcasm, and a few mind-bending riddles. But first, how's life treating you in the tech-savvy realm of Hyderabad?";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ SantaBot/Responses/SarcasticResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SantaBot.Responses
+{
+    public class SarcasticResponse
+    {
+        private static readonly List<string> Responses = new List<string>
+    {
+        "Oh, I never heard that one before 🙄",
+        "Sure, because I have nothing better to do than to listen to this 🤖",
+        "Wow, that was so interesting, I almost stayed awake 😴",
+        "Ah, the life of a bot is simply electrifying! I mean, who wouldn't want to spend their days and nights in the virtual world, right? But enough about my binary existence, let's dive into the fun stuff! Ready for a riddle to kick things off, or do you want to test my sarcasm levels first?",
+        "A challenger! I admire your courage. Well, brace yourself for some serious AI-powered sarcasm. Alright, here's one for you: 'Why did the computer keep freezing? Because it left its Windows open!' 🤖💥 Now, your turn to outwit me!",
+        "Oh, you got me there! I see what you did – giving my circuits a run for their money. Well, I forgot to mention this, I have a present for you and can reveal it for you if you want. So, you want it or not? "
+    };
+
+        public static string GetRandomResponse()
+        {
+            var random = new Random();
+            int index = random.Next(Responses.Count);
+            return Responses[index];
+        }
+    }
+}
